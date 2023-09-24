60f18d718b714b7cb075c38c33e315b205120b3e
Dumiya1111 <139087415+Dumiya1111@users.noreply.github.com>
2023-09-24 10:41:16 +0530

    Update config.js

--- config.js
+++ config.js
@@ -10,10 +10,10 @@ global.antitags = true;
 
 
 //auto functioner
-global.autoTyping = false;                //auto tying by default off.
+global.autoTyping = true;                //auto tying by default off.
 global.autoRecord = false;                //auto recording by default off.
 global.groupevent = false;                //This is the new variable for controlling group event handling.
-global.statusseen = true;                 //This is the new variable for controlling status seen.
+global.statusseen = false;                 //This is the new variable for controlling status seen.
 
 
 //
